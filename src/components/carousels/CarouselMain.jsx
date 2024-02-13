4bbfe8df7a274d5185f6e68b799ebe4f580c90ad
Jonathan Alexis Bello Lopez <jonathan.bellofg@gmail.com>
2024-02-13 14:30:35 -0600

    agregado inicio de carousel

---
+++ src/components/carousels/CarouselMain.jsx
@@ -0,0 +1,24 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/scss";
+import "swiper/scss/navigation";
+import "swiper/scss/pagination";
+
+const CarouselMain = ({ items }) => {
+  return (
+    <Swiper pagination={pagination} modules={[Pagination]} className="mySwiper">
+      <SwiperSlide>Slide 1</SwiperSlide>
+      <SwiperSlide>Slide 2</SwiperSlide>
+      <SwiperSlide>Slide 3</SwiperSlide>
+      <SwiperSlide>Slide 4</SwiperSlide>
+      <SwiperSlide>Slide 5</SwiperSlide>
+      <SwiperSlide>Slide 6</SwiperSlide>
+      <SwiperSlide>Slide 7</SwiperSlide>
+      <SwiperSlide>Slide 8</SwiperSlide>
+      <SwiperSlide>Slide 9</SwiperSlide>
+    </Swiper>
+  );
+};
+
+export default CarouselMain;
