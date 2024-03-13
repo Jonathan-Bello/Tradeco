a80c268bea6bb626f92de7e0464e5966f2406902
Jonathan Alexis Bello Lopez <jonathan.bellofg@gmail.com>
2024-03-13 13:15:06 -0600

    agregado boton de whatsapp fijo

--- src/components/carousels/CarouselMain/CarouselMain.jsx
+++ src/components/carousels/CarouselMain/CarouselMain.jsx
@@ -14,6 +14,7 @@ const CarouselMain = ({ items }) => {
       pagination={pagination}
       modules={[Pagination]}
       className="container !px-4 md:!mx-0 mt-8 mb-16 lg:mb-36"
+      autoplay={true}
       breakpoints={{
         slidesPerView: 1,
         spaceBetween: 8,
