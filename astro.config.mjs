d2f7c92bfd62439a40fa8eb1d16fcf3387cc46b2
Jonathan Alexis Bello Lopez <jonathan.bellofg@gmail.com>
2024-07-22 09:46:10 -0600

    add: new title in home and paragraph

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://tradeco506.com/',
   integrations: [tailwind(), react(), sitemap()],
   output: 'static'
 });
